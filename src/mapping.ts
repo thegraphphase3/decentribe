d1d21493f71d2da4c6b8e22f4714adb2297b2e71
thegraphphase3 <yulitapa11@gmail.com>
2020-12-10 21:53:50 +0100

    v10

--- src/mapping.ts
+++ src/mapping.ts
@@ -1,5 +1,5 @@
-import { Transfer } from '../generated/Decent/Decent'
-import { Transfertx } from '../generated/schema'
+import { Transfer, Approval } from '../generated/Decent/Decent'
+import { Transfertx, Approvaltx } from '../generated/schema'
 
 export function handleTransfer(event: Transfer): void {
   let id = event.params.to
@@ -8,8 +8,33 @@ export function handleTransfer(event: Transfer): void {
             .concat(event.logIndex.toString())
 
   let transfertx = new Transfertx(id)
+
   transfertx.from = event.params.from
   transfertx.to = event.params.to
   transfertx.value = event.params.value
+
+  transfertx.transaction = event.transaction.hash
+  transfertx.blockNumber = event.block.number
+  transfertx.blockTimestamp = event.block.timestamp
+
   transfertx.save()
+}
+
+export function handleApproval(event: Approval): void {
+  let id = event.params.owner
+            .toHexString()
+            .concat('-')
+            .concat(event.logIndex.toString())
+
+  let Approvaltx = new Approvaltx(id)
+
+  Approvaltx.owner = event.params.owner
+  Approvaltx.spender = event.params.spender
+  Approvaltx.value = event.params.value
+
+  Approvaltx.transaction = event.transaction.hash
+  Approvaltx.blockNumber = event.block.number
+  Approvaltx.blockTimestamp = event.block.timestamp
+
+  Approvaltx.save()
 }
